155335753feec1beadae8ccff0956efab5259c87
steveshumaker <88906397+steveshumaker@users.noreply.github.com>
2022-10-25 12:51:11 -0500

    Create hideScript.js

---
+++ hideScript.js
@@ -0,0 +1,8 @@
+function hideThing() {
+  var x = document.getElementById("eduSkills");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else{
+    x.style.display = "none";
+  }
+}
